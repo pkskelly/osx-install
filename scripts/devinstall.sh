1f3eddebedac73786ca1ac9a43e8f2acfae83982
Pete Skelly <peteskelly@gmail.com>
2017-11-03 21:18:46 -0400

    Update devinstall.sh
    Minor modifications and updates pre-install for new MBP (touch bar)

--- scripts/devinstall.sh
+++ scripts/devinstall.sh
@@ -1,21 +1,31 @@
 #!/bin/sh 
-brew install azure-cli
-brew cask install diffmerge
+
 brew cask install docker
+brew cask install dotnet-sdk
 brew cask install firefox 
 brew cask install ghost
 brew cask install iterm2
+brew install java
 brew install jsawk
 brew install jsdoc3
+brew cask install microsoft-azure-storage-explorer
+brew cask install ngrok
 brew cask install pencil
+brew cask install postman
 brew cask install smartgit 
 brew cask install wireshark
+brew cask install visual-studio
 brew cask install visual-studio-code
 brew cask install visual-studio-code-insiders
 brew cask install xmind
-brew cask install yarn
 brew install youtube-dl
 
+# yarn without dependencies 
+brew cask install yarn --ignore-dependencies
+
+# Azure ClI
+# brew install azure-cli
+curl -L https://aka.ms/InstallAzureCli | bash
 
 #databases
 brew cask install mongodb
