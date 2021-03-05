05372bf98f120b2f238158df5e582a33d2d31731
Pete Skelly <peteskelly@gmail.com>
2021-03-05 14:16:32 -0500

    Updates for temp laptop
    Remarked several items out to update for temp laptop

--- scripts/devinstall.sh
+++ scripts/devinstall.sh
@@ -5,19 +5,19 @@ brew cask install dotnet-sdk
 brew cask install firefox 
 brew cask install ghost
 brew cask install iterm2
-brew install java
-brew install jsawk
-brew install jsdoc3
+#brew install java
+#brew install jsawk
+#brew install jsdoc3
 brew cask install microsoft-azure-storage-explorer
 brew cask install ngrok
-brew cask install pencil
+#brew cask install pencil
 brew cask install postman
-brew cask install fork
-brew cask install smartgit 
-brew cask install sourcetree 
-brew cask install wireshark
+#brew cask install fork
+#brew cask install smartgit 
+#brew cask install sourcetree 
+#brew cask install wireshark
 brew cask install visual-studio
-brew cask install visual-studio-code
+#brew cask install visual-studio-code
 brew cask install visual-studio-code-insiders
 brew cask install xmind
 brew install youtube-dl
@@ -30,9 +30,9 @@ brew cask install yarn --ignore-dependencies
 curl -L https://aka.ms/InstallAzureCli | bash
 
 #databases
-brew cask install mongodb
-brew cask install robo-3t
-brew cask install db-browser-for-sqlite
+#brew cask install mongodb
+#brew cask install robo-3t
+#brew cask install db-browser-for-sqlite
 
 
 
