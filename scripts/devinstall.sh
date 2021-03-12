653de4dbf92c09c63c867f47712e31c156313867
Pete Skelly <peteskelly@gmail.com>
2021-03-12 18:22:40 -0500

    Updates for formulas for multiple casks
    Updates for formulas for multiple casks

--- scripts/devinstall.sh
+++ scripts/devinstall.sh
@@ -1,26 +1,19 @@
 #!/bin/sh 
 
-brew install cask docker
-brew install cask dotnet-sdk
-brew install cask firefox 
+brew install --cask docker
+brew install --cask dotnet-sdk
+brew install --cask firefox
 brew install cask ghost
 brew install cask iterm2
-#brew install java
-#brew install jsawk
-#brew install jsdoc3
-brew install cask microsoft-azure-storage-explorer
+brew install --cask microsoft-azure-storage-explorer
+brew install --cask azure-data-studio
 brew install --cask powershell
 brew install --cask obs
-brew install cask ngrok
-#brew install cask pencil
-brew install cask postman
-#brew install cask fork
-#brew install cask smartgit 
-#brew install cask sourcetree 
-#brew install cask wireshark
-brew install cask visual-studio
-#brew install cask visual-studio-code
-brew install cask visual-studio-code-insiders
+brew install --cask ngrok
+brew install --cask postman
+brew install --cask microsoft-edge
+brew install --cask visual-studio
+brew install --cask visual-studio-code
 brew install cask xmind
 brew install youtube-dl
 brew install gh 
@@ -48,4 +41,4 @@ brew install azure-functions-core-tools@3
 #databases
 #brew install cask mongodb
 #brew install cask robo-3t
-#brew install cask db-browser-for-sqlite
+brew install --cask db-browser-for-sqlite
